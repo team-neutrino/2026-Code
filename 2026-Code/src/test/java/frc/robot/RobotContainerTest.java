847cacaf9df60690c6c65ec77d5c17a34af71d2b
Andrew <gurellia53@gmail.com>
2026-01-17 10:55:38 -0600

    better assertion

--- 2026-Code/src/test/java/frc/robot/RobotContainerTest.java
+++ 2026-Code/src/test/java/frc/robot/RobotContainerTest.java
@@ -1,21 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj2.command.Command;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class RobotContainerTest {
   @BeforeEach
   void setup() {
-    assert HAL.initialize(500, 0);
+    assertTrue(HAL.initialize(500, 0));
   }
 
   @Test
