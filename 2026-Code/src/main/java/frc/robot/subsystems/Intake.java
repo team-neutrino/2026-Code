908f7312145b4982d82c29eb1ff3a3771c91c244
neutrino <neutrino3928@outlook.com>
2026-01-16 18:22:46 -0600

    done

--- 2026-Code/src/main/java/frc/robot/subsystems/Intake.java
+++ 2026-Code/src/main/java/frc/robot/subsystems/Intake.java
@@ -3,7 +3,10 @@ package frc.robot.subsystems;
 import static frc.robot.util.Constants.IntakeConstants.*;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,11 +17,21 @@ public class Intake extends SubsystemBase{
     private TalonFX m_deployMotor = new TalonFX(13, m_CANbus);
     private double m_rollerMotorVoltage;
     private double m_deployMotorVoltage;
-    // private double m_deployedAngle = DEPLOYED_ANGLE;
-    // private double m_motorAngle;
-    // Both above if encoder used
+    private TalonFXConfiguration m_motorConfig = new TalonFXConfiguration();
+    private final CurrentLimitsConfigs m_currentLimitConfig = new CurrentLimitsConfigs();
 
+    public Intake() {
+        m_currentLimitConfig.withSupplyCurrentLimit(CURRENT_LIMIT)
+            .withSupplyCurrentLimitEnable(true)
+            .withStatorCurrentLimit(CURRENT_LIMIT)
+            .withStatorCurrentLimitEnable(true);
+        m_motorConfig.CurrentLimits = m_currentLimitConfig;
 
+        m_rollerMotor.getConfigurator().apply(m_motorConfig);
+        m_deployMotor.getConfigurator().apply(m_motorConfig);
+        m_rollerMotor.setNeutralMode(NeutralModeValue.Coast);
+        m_deployMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
 
     @Override
     public void periodic() {
