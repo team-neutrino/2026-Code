bcfbf8a43b94a2b08aeb701d6f735f7eebb21cf1
Neutrino <neutrino3928@outlook.com>
2026-01-16 18:47:12 -0600

    Started climb command factory

---
+++ 2026-Code/src/main/java/frc/robot/command_factories/ClimbFactory.java
@@ -0,0 +1,15 @@
+package frc.robot.command_factories;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import static frc.robot.util.Subsystem.climb;
+import static frc.robot.Constants.ClimbConstants.*;
+
+public class ClimbFactory {
+    public static Command climbL1() {
+        return climb.moveClimbCommand(L1_POSITION);
+    }
+
+    public static Command climbDown() {
+        return climb.moveClimbCommand(GROUND_POSITION);
+    }
+}
--- 2026-Code/src/main/java/frc/robot/util/Subsystem.java
+++ 2026-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -3,5 +3,6 @@ package frc.robot.util;
 import frc.robot.subsystems.*;
 
 public class Subsystem {
+    public static final Climb climb = new Climb();   
     public static final Intake intake = new Intake();
 }
