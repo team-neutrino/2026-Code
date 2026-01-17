31172e71238739385e0daeb6c85e188555ebf80d
neutrino <neutrino3928@outlook.com>
2026-01-17 14:17:47 -0600

    Initialize limelight + rename constants
    also gitignore simgui json

--- 2026-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-Code/src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,7 @@ import static frc.robot.util.Subsystem.*;
 public class RobotContainer {
   private CommandXboxController m_buttonController = new CommandXboxController(1);
   private AlphaSubsystem m_subsystemContainer = new AlphaSubsystem();
+  private Subsystem m_Subsystem = new Subsystem();
   // private Subsystem m_subsystemContainer = new Subsystem();
   // comment out whichever subsystem container you're not testing
   private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
