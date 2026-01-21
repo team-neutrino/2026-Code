d8d5839f3177e2a957637f001d81037243e59578
Laptop 4 <neutrino3928@outlook.com>
2026-01-20 19:39:06 -0600

    colors based on hub status and blink on alliance shift

--- 2026-Code/src/main/java/frc/robot/subsystems/LED.java
+++ 2026-Code/src/main/java/frc/robot/subsystems/LED.java
@@ -7,10 +7,10 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StringTopic;
 import edu.wpi.first.networktables.StringPublisher;
+import frc.robot.util.HubActiveStatus;
+import frc.robot.util.Subsystem;
 
 public class LED extends SubsystemBase {
-
-    private String gameData;
     private double gameTime;
 
     private NetworkTableInstance m_nt = NetworkTableInstance.getDefault();
@@ -20,75 +20,47 @@ public class LED extends SubsystemBase {
     private final StringPublisher m_color_pub;
     private final StringPublisher m_state_pub;
 
+    private HubActiveStatus m_hub_status = Subsystem.hubState;
+
     public LED() {
         m_color_pub = m_color_topic.publish();
         m_state_pub = m_state_topic.publish();
     }
 
-    // LED Color indicates with alliance is activated
-    public void setAllianceColor() {
-        gameData = DriverStation.getGameSpecificMessage(); // update gameData
-        if (gameData.length() > 0) {
-            switch (gameData.charAt(0)) {
-                case 'B':
-                    // blue color turn on
-                    break;
-                case 'R':
-                    // red color turn on
-                    break;
-                default:
-                    // turn off
-                    break;
-            }
-        } else {
-            // turn off
-        }
-    }
-
-    public void setTransition() {
-        gameTime = DriverStation.getMatchTime();
-        if (gameTime < 0) {
-            // math
-
-        }
-
-    }
-
+    @Override
     public void periodic() {
-        setAllianceColor();
-        setTransition();
+        gameTime = DriverStation.getMatchTime();
 
-        System.out.println(DriverStation.getMatchTime());
+        // blink 5 seconds before alliance shift changes
+        if (gameTime <= 135 && gameTime >= 130) {
+            m_color_pub.set("white");
+            m_state_pub.set("blink");
+            return;
+        }
 
-        // zone shift. determine based on gameTime
-        //
-        // blink 3 seconds before transition (override) what color?
-        // m_state_pub.set("blink");
-        //
-        // blink during transition (override) what color?
-        // m_state_pub.set("blink");
+        else if (gameTime <= 110 && gameTime >= 105) {
+            m_color_pub.set("white");
+            m_state_pub.set("blink");
+            return;
+        }
 
-        // default to color of active hub. determine based on gameTime
-        //
-        // red active - red
-        // m_color_pub.set("red");
-        // m_state_pub.set("solid");
+        else if (gameTime <= 85 && gameTime >= 80) {
+            m_color_pub.set("white");
+            m_state_pub.set("blink");
+            return;
+        }
 
-        // blue active - blue
-        // m_color_pub.set("blue");
-        // m_state_pub.set("solid");
-        //
-        // both active - purple
-        // m_color_pub.set("purple");
-        // m_state_pub.set("solid");
+        else if (gameTime <= 60 && gameTime >= 55) {
+            m_color_pub.set("white");
+            m_state_pub.set("blink");
+            return;
+        }
 
-        // hopper full. determine based on sensor in hopper
-        //
-        // when beambreak is broken - orange
-        // m_color_pub.set("orange");
-        // m_state_pub.set("solid");
-
-        // hopper empty ? potentially deteremine based on a robot sensor
+        else if (gameTime <= 35 && gameTime >= 30) {
+            m_color_pub.set("white");
+            m_state_pub.set("blink");
+            return;
+        }
 
         // alignment to shoot/pass/climb. determine based on drive to point
         //
@@ -100,5 +72,30 @@ public class LED extends SubsystemBase {
         // m_color_pub.set("green");
         // m_state_pub.set("solid");
 
+        // hopper full. determine based on sensor in hopper
+        //
+        // when beambreak is broken - orange
+        // m_color_pub.set("orange");
+        // m_state_pub.set("solid");
+
+        // hopper empty ? potentially deteremine based on a robot sensor
+
+        // red hub active
+        if (m_hub_status.isRedHubActive() && !m_hub_status.isBlueHubActive()) {
+            m_color_pub.set("red");
+            m_state_pub.set("solid");
+        }
+
+        // blue hub active
+        else if (m_hub_status.isBlueHubActive() && !m_hub_status.isRedHubActive()) {
+            m_color_pub.set("blue");
+            m_state_pub.set("solid");
+        }
+
+        // default to purple (during auton, transition shift, endgame)
+        m_color_pub.set("purple");
+        m_state_pub.set("solid");
+
     }
+
 }
