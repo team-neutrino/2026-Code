6546e91b59cc95aebe8fa269cf1539737dc44730
neutrino <neutrino3928@outlook.com>
2026-01-26 17:26:03 -0600

    Made command that runs intake/outtake and deploy at the same time

--- 2026-Code/src/main/java/frc/robot/command_factories/IntakeFactory.java
+++ 2026-Code/src/main/java/frc/robot/command_factories/IntakeFactory.java
@@ -1,6 +1,7 @@
 package frc.robot.command_factories;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import static frc.robot.util.Constants.IntakeConstants.DEPLOY_VOLTAGE;
 import static frc.robot.util.Constants.IntakeConstants.INTAKE_VOLTAGE;
@@ -23,5 +24,17 @@ public class IntakeFactory {
 
     public static Command retractIntake() {
         return intake.deployIntake(RETRACT_VOLTAGE);
+    }
+
+    public static Command deployAndRunIntake() {
+        return new SequentialCommandGroup(
+                intake.deployIntake(DEPLOY_VOLTAGE),
+                intake.runIntake(INTAKE_VOLTAGE));
+    }
+
+    public static Command deployAndRunOuttake() {
+        return new SequentialCommandGroup(
+                intake.deployIntake(DEPLOY_VOLTAGE),
+                intake.runIntake(OUTTAKE_VOLTAGE));
     }
 }
