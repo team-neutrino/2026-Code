2ac6a22bf09bac297accdad0721466147df07a90
Andrew <gurellia53@gmail.com>
2026-01-27 18:09:03 -0600

    more precise, remove 30 deg shot

--- 2026-Code/src/main/java/frc/robot/util/Constants.java
+++ 2026-Code/src/main/java/frc/robot/util/Constants.java
@@ -1,7 +1,5 @@
 package frc.robot.util;
 
-import static edu.wpi.first.units.Units.Meter;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -9,8 +7,6 @@ import com.ctre.phoenix6.CANBus;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Distance;
 
 public class Constants {
     public static class RioConstants {
@@ -188,15 +184,15 @@ public class Constants {
 
         // Camera pose offsets
         public static final double AlphaBR_FORWARD_OFFSET = -0.1397;
-        public static final double AlphaBR_SIDE_OFFSET = 0.295;
-        public static final double AlphaBR_HEIGHT_OFFSET = 0.235;
+        public static final double AlphaBR_SIDE_OFFSET = 0.307975;
+        public static final double AlphaBR_HEIGHT_OFFSET = 0.238125;
         public static final double AlphaBR_ROLL_OFFSET = 180; // Roll (degrees)
         public static final double AlphaBR_PITCH_OFFSET = 30; // Pitch (degrees)
         public static final double AlphaBR_YAW_OFFSET = 180; // Yaw (degrees)
 
         public static final double AlphaBL_FORWARD_OFFSET = -0.1397; // Forward offset (meters)
-        public static final double AlphaBL_SIDE_OFFSET = -0.295; // Side offset (meters) right is positive
-        public static final double AlphaBL_HEIGHT_OFFSET = .23876; // Height offset (meters)
+        public static final double AlphaBL_SIDE_OFFSET = -0.307975; // Side offset (meters) right is positive
+        public static final double AlphaBL_HEIGHT_OFFSET = .254; // Height offset (meters)
         public static final double AlphaBL_ROLL_OFFSET = 0; // Roll (degrees)
         public static final double AlphaBL_PITCH_OFFSET = 30; // Pitch (degrees)
         public static final double AlphaBL_YAW_OFFSET = 180; // Yaw (degrees)
@@ -208,20 +204,14 @@ public class Constants {
         public static final double AlphaSHOOTER_PITCH_OFFSET = 15;
         public static final double AlphaSHOOTER_YAW_OFFSET = 0;
 
-        public static final double AlphaMINIMUM_XY_STD_DEV_LL4 = 1.1;
+        public static final double AlphaMINIMUM_XY_STD_DEV_LL4 = 0.15;
         public static final double AlphaMINIMUM_THETA_STD_DEV_LL4 = 9999999;
-        public static final double AlphaMINIMUM_XY_STD_DEV_LL3G = 1.9;
+        public static final double AlphaMINIMUM_XY_STD_DEV_LL3G = 0.15;
         public static final double AlphaMINIMUM_THETA_STD_DEV_LL3G = 9999999;
-        public static final double AlphaERROR_FACTOR_LL4 = 0.5;
+        public static final double AlphaERROR_FACTOR_LL4 = 0.05;
         public static final double AlphaERROR_FACTOR_LL4_ANGLE = 2;
-        public static final double AlphaERROR_FACTOR_LL3G = 0.9;
+        public static final double AlphaERROR_FACTOR_LL3G = 0.1;
         public static final double AlphaERROR_FACTOR_LL3G_ANGLE = 2;
-
-        public static final double BUMP_MINIMUM_THRESHOLD = 7;
-
-        public static final Distance ZERO = Distance.ofBaseUnits(0, Meter);
-        public static final Distance FIELD_DIMENSION_X = Distance.ofBaseUnits(Units.inchesToMeters(650.12), Meter);
-        public static final Distance FIELD_DIMENSION_Y = Distance.ofBaseUnits(Units.inchesToMeters(316.64), Meter);
     }
 
     public static class SwerveConstants {
@@ -244,23 +234,20 @@ public class Constants {
         public static final double SPLINE_MAX_ANGULAR_ACCELERATION = 4 * Math.PI;
         public static final double SPLINE_END_VELOCITY = 1.0;
         // Could be different multiples of 45
-        public static final Pose2d BLUE_PAST_BUMP_RIGHT = new Pose2d(5.7, 2.5, new Rotation2d(45));
-        public static final Pose2d BLUE_PAST_BUMP_LEFT = new Pose2d(5.7, 5.57, new Rotation2d(45));
-        public static final Pose2d RED_PAST_BUMP_LEFT = new Pose2d(10.84, 2.5, new Rotation2d(45));
-        public static final Pose2d RED_PAST_BUMP_RIGHT = new Pose2d(10.84, 5.57, new Rotation2d(45));
+        public static final Pose2d BLUE_PAST_BUMP_RIGHT = new Pose2d(5.742, 2.511, new Rotation2d(45));
+        public static final Pose2d BLUE_PAST_BUMP_LEFT = new Pose2d(5.742, 5.558, new Rotation2d(45));
+        public static final Pose2d RED_PAST_BUMP_LEFT = new Pose2d(10.799, 2.511, new Rotation2d(45));
+        public static final Pose2d RED_PAST_BUMP_RIGHT = new Pose2d(10.799, 5.558, new Rotation2d(45));
 
+        // Hub (4.626, 4.035)
         // Radius 3 meters
-        public static final Pose2d BLUE_CENTER_SHOT = new Pose2d(1.6, 4.03, new Rotation2d(0));
-        public static final Pose2d BLUE_RIGHT_SHOT = new Pose2d(2.47, 1.91, new Rotation2d(45));
-        public static final Pose2d BLUE_MID_RIGHT_SHOT = new Pose2d(2, 2.53, new Rotation2d(30));
-        public static final Pose2d BLUE_LEFT_SHOT = new Pose2d(2.47, 6.15, new Rotation2d(315));
-        public static final Pose2d BLUE_MID_LEFT_SHOT = new Pose2d(2, 5.53, new Rotation2d(330));
+        public static final Pose2d BLUE_CENTER_SHOT = new Pose2d(1.626, 4.035, new Rotation2d(0));
+        public static final Pose2d BLUE_RIGHT_SHOT = new Pose2d(2.505, 1.914, new Rotation2d(45));
+        public static final Pose2d BLUE_LEFT_SHOT = new Pose2d(2.505, 6.156, new Rotation2d(315));
 
-        public static final Pose2d RED_CENTER_SHOT = new Pose2d(14.94, 4.03, new Rotation2d(0));
-        public static final Pose2d RED_LEFT_SHOT = new Pose2d(14.07, 1.91, new Rotation2d(315));
-        public static final Pose2d RED_MID_LEFT_SHOT = new Pose2d(14.54, 2.53, new Rotation2d(330));
-        public static final Pose2d RED_RIGHT_SHOT = new Pose2d(14.07, 6.15, new Rotation2d(45));
-        public static final Pose2d RED_MID_RIGHT_SHOT = new Pose2d(14.54, 5.53, new Rotation2d(30));
+        public static final Pose2d RED_CENTER_SHOT = new Pose2d(14.915, 4.035, new Rotation2d(0));
+        public static final Pose2d RED_LEFT_SHOT = new Pose2d(14.036, 1.914, new Rotation2d(315));
+        public static final Pose2d RED_RIGHT_SHOT = new Pose2d(14.036, 6.156, new Rotation2d(45));
 
         // Climb positions (NEED TO BE UPDATED)
         public static final Pose2d BLUE_CLIMB_CENTER = new Pose2d(1, 4.03, new Rotation2d(0));
@@ -273,11 +260,9 @@ public class Constants {
 
         // Pose lists
         public static final List<Pose2d> BLUE_RADIAL_SHOOTING_POSES = List.of(BLUE_CENTER_SHOT, BLUE_RIGHT_SHOT,
-                BLUE_MID_RIGHT_SHOT,
-                BLUE_LEFT_SHOT, BLUE_MID_LEFT_SHOT);
+                BLUE_LEFT_SHOT);
         public static final List<Pose2d> RED_RADIAL_SHOOTING_POSES = List.of(RED_CENTER_SHOT, RED_RIGHT_SHOT,
-                RED_MID_RIGHT_SHOT,
-                RED_LEFT_SHOT, RED_MID_LEFT_SHOT);
+                RED_LEFT_SHOT);
 
         public static final List<Pose2d> BLUE_NEUTRAL_ZONE_POSES = List.of(BLUE_PAST_BUMP_RIGHT, BLUE_PAST_BUMP_LEFT);
         public static final List<Pose2d> RED_NEUTRAL_ZONE_POSES = List.of(RED_PAST_BUMP_RIGHT, RED_PAST_BUMP_LEFT);
@@ -295,5 +280,9 @@ public class Constants {
         public static final double ALLIANCE_ZONE_BLUE = 3.978;
         public static final double ALLIANCE_ZONE_RED = 12.563;
 
+    }
+
+    public static class AutonConstants {
+        public static final String CURRENT_AUTON = "Neutral Zone";
     }
 }
