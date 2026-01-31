eb6df1611db009727914716eefc4eeecc36fce58
Laptop 4 <neutrino3928@outlook.com>
2026-01-31 11:22:04 -0600

    dtpfinite for autons

--- 2026-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
+++ 2026-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
@@ -4,6 +4,9 @@
 
 package frc.robot.command_factories;
 
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import static frc.robot.util.Constants.ShooterConstants.SOFT_SHOT_ANGLE;
 import static frc.robot.util.Constants.ShooterConstants.SOFT_SHOT_SPEED;
@@ -30,5 +33,9 @@ public class SuperstructureFactory {
         DriveToPoint drive = new DriveToPoint(SHOOT_POSES);
         return drive.until(() -> AlphaSubsystem.swerve.getCurrentPose().getX() - drive.getTarget().getX() <= .1
                         && AlphaSubsystem.swerve.getCurrentPose().getY() - drive.getTarget().getY() <= .1);
+    public static Command DriveToPointFinite(List<Pose2d> targetPoseList) {
+        DriveToPoint drive = new DriveToPoint(targetPoseList);
+        return drive.until(() -> AlphaSubsystem.swerve.getCurrentPose().getX() - drive.getTarget().getX() <= .05
+                && AlphaSubsystem.swerve.getCurrentPose().getY() - drive.getTarget().getY() <= .05);
     }
 }
