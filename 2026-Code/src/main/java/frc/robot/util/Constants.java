03b5305ac220c27345d749368d14826c01e8283f
yehang <theduckmasteryc@gmail.com>
2026-02-04 21:16:27 -0600

    new climb commands

--- 2026-Code/src/main/java/frc/robot/util/Constants.java
+++ 2026-Code/src/main/java/frc/robot/util/Constants.java
@@ -222,7 +222,7 @@ public class Constants {
 
     public static class DriveToPointConstants {
         public enum TargetMode {
-            SHOOTING, SHUTTLING, CLIMBING
+            SHOOTING, SHUTTLING, CLIMBING, AUTOCLIMBING
         }
 
         public static final double GAIN_SCHEDULE_THRESHOLD = 0.1;
