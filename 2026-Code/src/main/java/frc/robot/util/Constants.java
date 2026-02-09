5e3d79d330669e2c0d5f0eac325d353ee9711438
Laptop 4 <neutrino3928@outlook.com>
2026-02-09 17:53:34 -0600

    intake tested

--- 2026-Code/src/main/java/frc/robot/util/Constants.java
+++ 2026-Code/src/main/java/frc/robot/util/Constants.java
@@ -133,19 +133,19 @@ public class Constants {
         public static final int ROLLER_MOTOR_ID = 12;
         public static final int DEPLOY_MOTOR_ID = 13;
 
-        public static final double CURRENT_LIMIT = 10;
+        public static final double CURRENT_LIMIT = 20;
 
         public static final double INTAKE_VOLTAGE = 5;
         public static final double OUTTAKE_VOLTAGE = -5;
 
-        public static final double STARTING_POSITION = 0;
-        public static final double DEPLOYED_POSITION = 80; // all positions and allowed errors are placeholders
+        public static final double STARTING_POSITION = -.50;
+        public static final double DEPLOYED_POSITION = -8.5;
         public static final double INTERMEDIATE_POSITION_1 = 60;
         public static final double INTERMEDIATE_POSITION_2 = 30;
         public static final double ALLOWED_TARGET_ERROR = 4;
         public static final double ALLOWED_INTAKE_ERROR = 4;
 
-        public static final double INTAKE_kP = 0.1; // PID values are all placeholders
+        public static final double INTAKE_kP = 2;
         public static final double INTAKE_kI = 0.0;
         public static final double INTAKE_kD = 0.0;
     }
