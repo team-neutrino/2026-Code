a9c963e45a31c8dc2989a640f0832d66374c032b
neutrino <neutrino3928@outlook.com>
2026-02-21 13:11:21 -0600

    path

--- 2026-Code/src/main/java/frc/robot/util/Constants.java
+++ 2026-Code/src/main/java/frc/robot/util/Constants.java
@@ -372,6 +372,6 @@ public class Constants {
     }
 
     public static class AutonConstants {
-        public static final String CURRENT_AUTON = "Neutral Zone 2";
+        public static final String CURRENT_AUTON = "Neutral Near L";
     }
 }
