50b06ddca0754e7f3168e30cb5bc0f5dc9352d14
neutrino <neutrino3928@outlook.com>
2026-02-21 14:27:14 -0600

    shooterDefault

--- 2026-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-Code/src/main/java/frc/robot/RobotContainer.java
@@ -66,6 +66,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("runIntake", IntakeFactory.runIntake());
     NamedCommands.registerCommand("feedShooter", SuperstructureFactory.FeedShooter());
     NamedCommands.registerCommand("noShoot", SuperstructureFactory.noShooting());
+    NamedCommands.registerCommand("shoooterDefault", shooter.defaultCommand());
   }
 
   public Command getAutonomousCommand() {
