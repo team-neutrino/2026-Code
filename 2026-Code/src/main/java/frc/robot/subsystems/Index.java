c8cbe55589e4fe47e0a3216c0579c57d5f6ce7bc
neutrino <neutrino3928@outlook.com>
2026-02-23 18:02:00 -0600

    deleted kicker
    whole subsystem to be called index

--- 2026-Code/src/main/java/frc/robot/subsystems/Index.java
+++ 2026-Code/src/main/java/frc/robot/subsystems/Index.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.Constants.RioConstants;
 
-import static frc.robot.util.Constants.IndexerConstants.*;
+import static frc.robot.util.Constants.IndexConstants.*;
 import static frc.robot.util.Subsystems.shooterArbiter;
 
 public class Index extends SubsystemBase {
