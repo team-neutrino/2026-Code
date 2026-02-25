a4c5ef3bb2553c60ca09b062cb2a9b517a3727e2
Vienna Rossmanith <887432ros@ames.k12.ia.us>
2026-02-25 17:09:08 -0600

    rebase fixes

--- 2026-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
+++ 2026-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
@@ -16,7 +16,7 @@ public class SuperstructureFactory {
     }
 
     public static Command spitFuel() {
-        return kicker.kickWhenPress().alongWith(index.spinWhenPress())
+        return index.kickWhenPress().alongWith(index.spinWhenPress())
                 .alongWith(shooter.runShooterAndHood(SOFT_SHOT_SPEED, SOFT_SHOT_ANGLE));
     }
 
