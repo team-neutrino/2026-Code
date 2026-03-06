40013da50b5a189b9953c546022629c30904ad5c
Vienna Rossmanith <887432ros@ames.k12.ia.us>
2026-03-06 09:59:38 -0600

    auton adjustments untested

--- 2026-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-Code/src/main/java/frc/robot/RobotContainer.java
@@ -62,6 +62,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("DriveToPointFinite", SuperstructureFactory.DriveToPointFinite(SHOOT_POSES));
     NamedCommands.registerCommand("DriveToPoint", new DriveToPoint(SHOOT_POSES, false));
     NamedCommands.registerCommand("deployAndRunIntake", IntakeFactory.deployAndRunIntake());
+    NamedCommands.registerCommand("retractIntake", intake.retractIntake());
     NamedCommands.registerCommand("feedShooter", index.feedShooter());
     NamedCommands.registerCommand("noShoot", index.noKickAndSpin());
     NamedCommands.registerCommand("shooterDefault", shooter.defaultCommand());
