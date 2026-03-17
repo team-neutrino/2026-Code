ea3445ac518e5136373a89ee03f48b4c3f861af6
neutrino <neutrino3928@outlook.com>
2026-03-17 17:41:43 -0500

    changed to toggle

--- 2026-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2026-Code/src/main/java/frc/robot/RobotContainer.java
@@ -53,7 +53,7 @@ public class RobotContainer {
     m_buttonController.b().whileTrue(SuperstructureFactory.shuttle());
     m_buttonController.x().whileTrue(index.feedShooter());
     m_buttonController.y().whileTrue(SuperstructureFactory.spitFuel());
-    m_buttonController.povUp().onTrue(IntakeFactory.shakeHopper().repeatedly());
+    m_buttonController.povUp().toggleOnTrue(IntakeFactory.shakeHopper().repeatedly());
     m_buttonController.leftTrigger().onTrue(IntakeFactory.toggleIntake());
     m_buttonController.rightTrigger().whileTrue(SuperstructureFactory.shootOnly());
     m_buttonController.leftBumper().whileTrue(IntakeFactory.runIntake());
