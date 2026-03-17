c93246642b1ab2ead3a47af0ccf6612197b21a15
neutrino <neutrino3928@outlook.com>
2026-03-17 18:55:08 -0500

    Tested

--- 2026-Code/src/main/java/frc/robot/subsystems/Swerve.java
+++ 2026-Code/src/main/java/frc/robot/subsystems/Swerve.java
@@ -284,14 +284,12 @@ public class Swerve extends CommandSwerveDrivetrain {
     }
 
     private void checkEngageBrake(double forward, double left, double rotation) {
-        System.out.println(forward + " " + left + " " + rotation);
         if (!m_brakeEngaged && Math.abs(forward) < BRAKE_ALLOWED_ERROR && Math.abs(left) < BRAKE_ALLOWED_ERROR
                 && Math.abs(rotation) < BRAKE_ALLOWED_ERROR
                 && getSpeedMetersPerSecond() < START_BRAKING_VELOCITY) {
             m_brakeEngaged = true;
         } else if (Math.abs(forward) > BRAKE_ALLOWED_ERROR || Math.abs(left) > BRAKE_ALLOWED_ERROR
                 || Math.abs(rotation) > BRAKE_ALLOWED_ERROR) {
-            System.out.println("hererererererere");
             m_brakeEngaged = false;
         }
     }
@@ -334,7 +332,6 @@ public class Swerve extends CommandSwerveDrivetrain {
             double magnitude = Math.hypot(forward, left) * MAX_SPEED;
             magnitude = m_slewLimit.calculate(magnitude);
             checkEngageBrake(forward, left, rotation);
-            System.out.println(m_brakeEngaged);
 
             if (m_brakeEngaged) {
                 setControl(SwerveRequestStash.brake);
