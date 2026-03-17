0ba719041251768ab386c5617fd5e091ef8c9191
Hanna <hanna.k301@gmail.com>
2026-03-16 20:11:34 -0500

    tried testing, values still need tuning

--- 2026-Code/src/main/java/frc/robot/util/Constants.java
+++ 2026-Code/src/main/java/frc/robot/util/Constants.java
@@ -168,8 +168,8 @@ public class Constants {
 
         public static final double STARTING_POSITION = -.50;
         public static final double DEPLOYED_POSITION = -8.5;
-        public static final double INTERMEDIATE_POSITION_1 = -3; // placeholder
-        public static final double INTERMEDIATE_POSITION_2 = -5; // placeholder
+        public static final double INTERMEDIATE_POSITION_1 = -6; // placeholder
+        public static final double INTERMEDIATE_POSITION_2 = -8; // placeholder
         public static final double ALLOWED_TARGET_ERROR = 4;
 
         public static final double INTAKE_kP = 2;
