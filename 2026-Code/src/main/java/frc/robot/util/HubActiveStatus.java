83442bbc4d654ad8f5523e19f025d39ebb7bdb03
Vienna Rossmanith <887432ros@ames.k12.ia.us>
2026-03-19 17:56:59 -0500

    wip
    no more inheritance

--- 2026-Code/src/main/java/frc/robot/util/HubActiveStatus.java
+++ 2026-Code/src/main/java/frc/robot/util/HubActiveStatus.java
@@ -1,9 +1,8 @@
 package frc.robot.util;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class HubActiveStatus extends SubsystemBase {
+public class HubActiveStatus {
     public enum Alliance {
         RED,
         BLUE
@@ -74,12 +73,11 @@ public class HubActiveStatus extends SubsystemBase {
     public Alliance whoWonFirstAuton() {
         return inactiveFirst;
     }
-    
-    public Alliance getAlliance(){
-        if(DriverStation.getAlliance().get() == DriverStation.Alliance.Red){
+
+    public Alliance getAlliance() {
+        if (DriverStation.getAlliance().get() == DriverStation.Alliance.Red) {
             return Alliance.RED;
-        }
-        else{
+        } else {
             return Alliance.BLUE;
         }
     }
