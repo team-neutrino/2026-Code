f5dbb10dee4b1e98de3e28c3c9eaa9edf9c222f7
Andrew G <gurellia53@gmail.com>
2026-03-24 14:38:39 -0700

    on bump doesn't care about multi tags (#266)
    * on bump doesn't care about multi tags
    
    * copilot

--- 2026-Code/src/main/java/frc/robot/subsystems/Vision.java
+++ 2026-Code/src/main/java/frc/robot/subsystems/Vision.java
@@ -423,15 +423,15 @@ public class Vision extends SubsystemBase {
 
         /** Computes XY standard deviation using tag distance and count. */
         private double setXYstdev(double distance, double numberOfTags, int numberOfHubTags) {
-            double errorFactor = getErrorFactor();
             double minimumXyStdDev = getMinimumStdDev();
+            if (onBump()) {
+                return minimumXyStdDev;
+            }
+
+            double errorFactor = getErrorFactor();
             if (numberOfHubTags < 2) {
                 errorFactor *= 10.0;
                 minimumXyStdDev *= 10.0;
-            }
-
-            if (onBump()) {
-                return minimumXyStdDev;
             }
 
             return Math.max(
@@ -469,7 +469,10 @@ public class Vision extends SubsystemBase {
                     .setNumber(throttle);
         }
 
-        /** @return true if the robot's current field pose (X/Y) lies within the defined bump region. */
+        /**
+         * @return true if the robot's current field pose (X/Y) lies within the defined
+         *         bump region.
+         */
         public boolean onBump() {
             double poseX = swerve.getCurrentPose().getX();
             double poseY = swerve.getCurrentPose().getY();
