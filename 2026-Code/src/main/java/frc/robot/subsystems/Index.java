0e3d40acebd749d6cfec7c9cacd188100250d1bf
neutrino <neutrino3928@outlook.com>
2026-04-03 20:05:19 -0500

    meh

--- 2026-Code/src/main/java/frc/robot/subsystems/Index.java
+++ 2026-Code/src/main/java/frc/robot/subsystems/Index.java
@@ -129,6 +129,7 @@ public class Index extends SubsystemBase {
     @Override
     public void periodic() {
         checkEmptyHopper();
+        System.out.println(isHopperEmpty());
         calculateBallsPerSecond();
         setSpindexerVoltage();
         setKickerVoltage();
