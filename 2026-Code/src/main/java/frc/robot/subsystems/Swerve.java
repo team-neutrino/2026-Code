ef82660197db217eecc0489b5db1ad7d0b88a420
neutrino <neutrino3928@outlook.com>
2026-04-13 20:53:04 -0500

    WIP on auto

--- 2026-Code/src/main/java/frc/robot/subsystems/Swerve.java
+++ 2026-Code/src/main/java/frc/robot/subsystems/Swerve.java
@@ -280,10 +280,10 @@ public class Swerve extends CommandSwerveDrivetrain {
     }
 
     private void configurePathPlanner() {
-        double pTranslation = 1;
+        double pTranslation = 6;
         double iTranslation = 0;
         double dTranslation = 0;
-        double pRotation = 1;
+        double pRotation = 3;
         double iRotation = 0;
         double dRotation = 0;
         PIDConstants translationConstants = new PIDConstants(pTranslation, iTranslation, dTranslation);
