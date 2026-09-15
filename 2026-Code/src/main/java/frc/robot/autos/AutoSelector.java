ec0bc28d11b767355463c9fb29be9b17fbdf657c
Laptop 4 <neutrino3928@outlook.com>
2026-09-15 18:03:56 -0500

    change that wasn't committed
    for THE jh

--- 2026-Code/src/main/java/frc/robot/autos/AutoSelector.java
+++ 2026-Code/src/main/java/frc/robot/autos/AutoSelector.java
@@ -7,13 +7,14 @@ import choreo.auto.AutoChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.autos.trajectories.test;
 import frc.robot.util.Subsystems;
 
 public class AutoSelector {
-  private AutoSelector m_autoChooser = new AutoSelector();
+  private AutoChooser m_autoChooser = new AutoChooser();
 
   public AutoSelector() {
-    m_autoChooser.addRoutine("Example", () -> example());
+    m_autoChooser.addRoutine("Example", () -> test.example());
 
     SmartDashboard.putData("AutoChooser", m_autoChooser);
   }
